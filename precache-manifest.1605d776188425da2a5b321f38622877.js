585aaca9200ab131ed6cec6572ad0254d0ef3728
Filipe Herculano <fifo_@live.com>
2020-03-27 19:06:57 -0400

    Updates

---
+++ precache-manifest.1605d776188425da2a5b321f38622877.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3243dc1dc25ba59eba4375abf720bcf",
+    "url": "/jutsu/index.html"
+  },
+  {
+    "revision": "a6812471f2f1a3df87d9",
+    "url": "/jutsu/static/css/main.decb6672.chunk.css"
+  },
+  {
+    "revision": "605bf90015f233c67685",
+    "url": "/jutsu/static/js/2.16dedb4a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jutsu/static/js/2.16dedb4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6812471f2f1a3df87d9",
+    "url": "/jutsu/static/js/main.0eb54c01.chunk.js"
+  },
+  {
+    "revision": "de7a2624e6cec1be6f62",
+    "url": "/jutsu/static/js/runtime-main.d956e946.js"
+  }
+]);
